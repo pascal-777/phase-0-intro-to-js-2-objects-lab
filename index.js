fdc86c33bb0ed6518c24651507752b044ebdab8f
pascal <pascal.nicintije@student.moringaschool.com>
2024-03-17 15:39:17 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Pascal",
+    adress: 254,
+}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+const newO = updateEmployeeWithKeyAndValue(employee, "name2", "effiecancode");
+console.log(newO);
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+const newOb = destructivelyUpdateEmployeeWithKeyAndValue(employee, "adress2", "255");
+console.log(newOb);
+function deleteFromEmployeeByKey(obj, key) {
+    const newObje = {...obj};
+    delete newObje[key];
+    return newObje;
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+
+    return obj;
+}
